8b6cf2a08e9a2ee8d63e44e79ad04b04a75eda65
Ricky <rickbojorquez@gmail.com>
2019-01-21 22:55:28 -0500

    Initial commit

---
+++ app/src/main/java/com/example/week3day1/MusicController.java
@@ -0,0 +1,13 @@
+package com.example.week3day1;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+public class MusicController extends MediaController {
+
+    public MusicController(Context c){
+        super(c);
+    }
+
+    public void hide(){}
+}
